b584b9dd174c788ca1a8caa476c354c20720d00f
NatalyaKhrol <tashapas@yahoo.com>
2022-09-11 23:24:04 +0300

    Some mistakes were corrected

--- src/test/java/pages/CreateAccountPage.java
+++ src/test/java/pages/CreateAccountPage.java
@@ -18,13 +18,14 @@ public class CreateAccountPage extends BasePage {
         driver.get(BASE_URL + "lightning/o/Account/list?filterName=Recent");
         wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath
                 ("//span[text()='Accounts' and @class='slds-var-p-right_x-small']")));
+        driver.findElement(By.cssSelector("[title='Accounts']")).click();
     }
 
     public void createAccount(){
         driver.findElement(NEW_BUTTON).click();
     }
     public void create(String rating, String accountName, String phone, String fax, String accountNumber, String type,
-                       String billingStreet, String shippingCity) {
+                       String billingStreet, String shippingStreet) {
         new Dropdown("Rating", driver).select(rating);
         new Input("Account Name", driver).write(accountName);
         new Input("Phone", driver).write(phone);
@@ -32,7 +33,7 @@ public class CreateAccountPage extends BasePage {
         new Input("Account Number", driver).write(phone);
         new  Dropdown("Type", driver).select(type);
         new TextArea("Billing Street", driver).write(billingStreet);
-        new TextArea("Shipping City", driver).write(shippingCity);
+        new TextArea("Shipping Street", driver).write(shippingStreet);
 
     }
 }
